5f89d50729e5f5cc85a717fafb51b87489d8e78a
Tobias Trobisch <tobias.trobisch@gmail.com>
2023-08-30 14:48:07 +0200

    added bitrate and duration as params

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,27 @@ use serde_json;
 
 mod iperf_result;
 
-#[derive(Deserialize)]
-struct QueryInfo {
+#[derive(Deserialize, Debug)]
+struct ProbeOptions {
     target: String,
+    #[serde(default = "default_bitrate")]
+    bitrate: String,
+    #[serde(default = "default_duration")]
+    duration: String,
+}
+
+fn default_bitrate() -> String {
+    "0".to_string()
+}
+
+fn default_duration() -> String {
+    "5".to_string()
 }
 
 #[get("/probe")]
-async fn probe(params: web::Query<QueryInfo>, lock: web::Data<sync::Arc<sync::Mutex<()>>>) -> actix_web::HttpResponse {
+async fn probe(params: web::Query<ProbeOptions>, lock: web::Data<sync::Arc<sync::Mutex<()>>>) -> actix_web::HttpResponse {
     let _guard = lock.lock();
-    let metrics = execute_iperf3(params.target.clone().as_str());
+    let metrics = execute_iperf3(&params);
     match metrics {
         None => {
             println!("No metrics");
@@ -42,27 +54,31 @@ async fn main() -> std::io::Result<()> {
         .await
 }
 
-fn execute_iperf3<'a>(server: &'a str) -> Option<iperf_result::IperfResult> {
+fn execute_iperf3(server: &ProbeOptions) -> Option<iperf_result::IperfResult> {
     let output = Command::new("iperf3")
         .arg("-J")
         .arg("-u")
         .arg("-b")
-        .arg("0")
+        .arg(server.bitrate.as_str())
         .arg("-t")
-        .arg("5")
+        .arg(server.duration.as_str())
         .arg("-c")
-        .arg(server)
+        .arg(server.target.as_str())
         .output()
         .expect("Failed to execute iperf3");
 
-    let output = String::from_utf8(output.stdout).expect("Invalid JSON");
+    let json_string = String::from_utf8(output.stdout).expect("Invalid JSON");
 
-    serde_json::from_str(output.as_str()).ok()
+    serde_json::from_str(json_string.as_str()).ok()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{execute_iperf3, ProbeOptions};
+
     #[test]
     fn test_probe() {
+        let options = ProbeOptions{ duration: "5".to_string(), bitrate: "0".to_string(), target: "172.16.0.16".to_string() };
+        let iperf_result = execute_iperf3(&options).unwrap();
     }
 }
